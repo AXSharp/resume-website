218bd12dafd175566569e2381b471ccdd9836e13
Artis Stepens <84177304+AXSharp@users.noreply.github.com>
2023-03-15 21:16:39 +0200

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,47 @@
 <template>
-  <header>
-    <div class="wrapper">
-      <div class="title">
-        <h2 class="name">Artis Stepens</h2>
-        <p class="occupation">Mulesoft developer</p>
-      </div>
-      <div class="title">
-        <div class="child_title">
-          <img class="profileImg" src="./assets/titleImage.jpg" alt="" />
+  <div class="wrapper">
+    <div class="title">
+      <div class="titleNav">
+        <div>
+          <h2 class="name">Artis Stepens</h2>
+          <p class="occupation">Mulesoft developer</p>
         </div>
-        <div class="child_title">
-          <h2 class="about">A little bit about me:</h2>
-          <p>
-            Hi there! I'm a Mulesoft Developer at Intrum Global Technologies,
-            passionate about creating innovative solutions and enhancing the
-            user experience. When I'm not working, you can find me playing
-            volleyball or indulging in my love for video games such as Rainbow Six
-            siege and Valheim. I also have two adorable cats, one of which I
-            rescued from the streets. With a year of experience in Mulesoft
-            development, I'm always looking for new challenges and opportunities
-            to expand my skill set.
-          </p>
+        <div class="navbar">
+          <ul class="md:flex md:items-center">
+            <li class="md:mx-4">Resume</li>
+            <li>Experience</li>
+            <li>Contact me</li>
+          </ul>
         </div>
       </div>
     </div>
-  </header>
-</template>
 
+    <div class="title">
+      <div class="child_title">
+        <img class="profileImg" src="./assets/titleImage.jpg" alt="" />
+      </div>
+      <div class="child_title">
+        <h2 class="about">A little bit about me:</h2>
+        <p>
+          Hi there! I'm a Junior Mulesoft Developer at Intrum Global
+          Technologies, passionate about creating innovative solutions and
+          enhancing the user experience. With a year of experience in Mulesoft
+          development, I'm always looking for new challenges and opportunities
+          to expand my skill set.
+        </p>
+        <p>
+          When I'm not working, you can find me playing volleyball or indulging
+          in my love for video games such as Rainbow Six siege and Valheim. I
+          also have two adorable cats, one of which I rescued from the streets.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
 <style scoped>
 .title {
   margin: auto;
-  width: 50%;
+  width: 60%;
   line-height: 35px;
   overflow: hidden;
   background-color: #294c60;
@@ -58,6 +69,7 @@ p {
   font-family: "Inconsolata", monospace;
   font-size: 15pt;
   opacity: 100%;
+  margin-bottom: 30px;
 }
 .name {
   font-size: 30pt;
@@ -66,17 +78,36 @@ p {
 .occupation {
   margin-left: 50px;
   font-size: 15pt;
+  margin-bottom: 0px;
 }
 
 .profileImg {
-  width: 100%;
+  width: 90%;
   border-radius: 50%;
   align-content: center;
 }
 .wrapper {
   padding-top: 30px;
-  border-color: green;
-  border-style: solid;
-  border-width: medium;
+}
+.titleNav {
+  display: flex;
+}
+.navbar {
+  justify-content: flex-end;
+  display: flex;
+  width: 60%;
+  font-family: "Inconsolata", monospace;
+  font-size: 15pt;
+}
+ul {
+  display: flex;
+  align-items: right;
+  list-style-type: none;
+}
+li {
+  padding-left: 20px;
+}
+li:hover {
+  color: #FFC49B;
 }
 </style>
