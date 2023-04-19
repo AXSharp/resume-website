5b1dc2ee4a04b8615eb48445a0cd77f4b4b2f195
Artis Stepens <84177304+AXSharp@users.noreply.github.com>
2023-04-19 23:34:57 +0300

    created a transition for the header

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Add class to header on scroll
 window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > 0);
-  });
+  const header = document.querySelector('header');
+  header.classList.toggle('scrolled', window.scrollY > 0);
+});
+
+window.addEventListener('scroll', function() {
+  const h2 = document.querySelector('h2');
+  h2.classList.toggle('scrolled', window.scrollY > 0);
+});
+
+window.addEventListener('scroll', function() {
+  const p = document.querySelector('p');
+  p.classList.toggle('scrolled', window.scrollY > 0);
+});
